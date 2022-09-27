d7e09c4abe0d33563755e69ee172c41f8d688250
Mehdi Hussain <mehdi@mehdi.us>
2022-09-27 17:55:12 -0400

    Initial Commit

---
+++ ReadyMade/ReadyMadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadyMadeApp.swift
+//  ReadyMade
+//
+//  Created by Mehdi Hussain on 9/27/22.
+//
+
+import SwiftUI
+
+@main
+struct ReadyMadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
